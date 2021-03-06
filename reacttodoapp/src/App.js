3dda0296ad2d379846cef68c37c410e19bd12fd1
EsmondKim <ekelectronicmail@gmail.com>
2021-03-06 17:25:10 -0600

    Toggle reminder function working.

--- reacttodoapp/src/App.js
+++ reacttodoapp/src/App.js
@@ -27,14 +27,19 @@ function App() {
 //delete task
 const deleteTask = (id) => {
   setTasks(tasks.filter((task) => task.id !== id  )) 
+}
 
-}  
+//Toggle Reminder
+const toggleReminder = (id) => {
+  setTasks(tasks.map((task) => task.id ===id 
+  ? { ...task, reminder: !task.reminder} : task))
+}
 
 return (
     <div className="container">
       <Header title="Task Tracker"/>
       {tasks.length > 0 ? <Tasks tasks={tasks} 
-      onDelete={deleteTask} /> : "No tasks to show"}
+      onDelete={deleteTask} onToggle={toggleReminder} /> : "No tasks to show"}
     </div>
   )
 }
