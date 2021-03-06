ff2f0d4e4fc2054bd14bc5ba1c4f776f6c815859
EsmondKim <ekelectronicmail@gmail.com>
2021-03-06 15:40:42 -0600

    Added header and button components.

--- reacttodoapp/src/App.js
+++ reacttodoapp/src/App.js
@@ -1,10 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from "./components/Header";
 
 function App() {
   return (
-    <div className="App">
-      Hello from React World!
+    <div className="container">
+      <Header title="Task Tracker"/>
     </div>
   );
 }
